423d59a7981ffc6f67691bf19c07c5893ad160a1
Kazutaka <kaznak.at.work@gmail.com>
2019-11-05 13:49:32 +0900

    fix(cloud-res): ssl certs setting

--- cloud-res/34.lb_listener.tf
+++ cloud-res/34.lb_listener.tf
@@ -22,7 +22,6 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.main.arn
   port              = 443
   protocol          = "HTTPS"
-  certificate_arn   = local.listener_certs[0]
 
   default_action {
     type             = "forward"
@@ -31,7 +30,7 @@ resource "aws_lb_listener" "https" {
 }
 
 resource "aws_lb_listener_certificate" "main" {
-  count           = length(local.listener_certs) - 1
+  count           = length(local.listener_certs)
   listener_arn    = aws_lb_listener.https.arn
-  certificate_arn   = local.listener_certs[count.index + 1]
+  certificate_arn   = local.listener_certs[count.index]
 }
